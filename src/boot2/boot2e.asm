0d1bf57b957e34c42a554375d6fc08ed45aeaec7
mebrahimaleem <mebrahimaleem@gmail.com>
2024-11-26 21:56:37 -0800

    Updated license

--- src/boot2/boot2e.asm
+++ src/boot2/boot2e.asm
@@ -1,6 +1,19 @@
 ;boot2e.asm
+; MIT License
 
-;This is the entry point for boot2. This file will be linked with the remainaing kernel so that we can switch from assembly to C.
+; Copyright 2022-2024 Ebrahim Aleem
+; 
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+; 
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+; 
+; ;This is the entry point for boot2. This file will be linked with the remainaing kernel so that we can switch from assembly to C.
 
 ;NOTE: Since we are linking this file with ld, there is no need for an ORG directive
 
